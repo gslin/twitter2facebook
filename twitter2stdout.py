15e4f7d461f9ea03f8f89787e1ea5401f35e33c4
Gea-Suan Lin <gslin@gslin.org>
2020-05-30 05:09:15 +0800

    Use class.

--- twitter2stdout.py
+++ twitter2stdout.py
@@ -6,23 +6,24 @@ import os
 import sqlite3
 import twitter
 
-def work():
-    home = os.environ['HOME']
-    f_conf = '{}/.config/twitter2facebook/config.ini'.format(home)
-    f_db = '{}/.config/twitter2facebook/entry.sqlite3'.format(home)
+class Twitter2Stdout(object):
+    def main():
+        home = os.environ['HOME']
+        f_conf = '{}/.config/twitter2facebook/config.ini'.format(home)
+        f_db = '{}/.config/twitter2facebook/entry.sqlite3'.format(home)
 
-    c = configparser.ConfigParser()
-    c.read(f_conf)
+        c = configparser.ConfigParser()
+        c.read(f_conf)
 
-    t_ak = c['default']['twitter_access_token_key']
-    t_as = c['default']['twitter_access_token_secret']
-    t_ck = c['default']['twitter_consumer_key']
-    t_cs = c['default']['twitter_consumer_secret']
-    t_user = c['default']['twitter_username']
-    t = twitter.Api(access_token_key=t_ak, access_token_secret=t_as, consumer_key=t_ck, consumer_secret=t_cs)
+        t_ak = c['default']['twitter_access_token_key']
+        t_as = c['default']['twitter_access_token_secret']
+        t_ck = c['default']['twitter_consumer_key']
+        t_cs = c['default']['twitter_consumer_secret']
+        t_user = c['default']['twitter_username']
+        t = twitter.Api(access_token_key=t_ak, access_token_secret=t_as, consumer_key=t_ck, consumer_secret=t_cs)
 
-    for status in sorted(list(t.GetUserTimeline(screen_name=t_user)), key=lambda x: x.id):
-        print('* tweet = {}'.format(status))
+        for status in sorted(list(t.GetUserTimeline(screen_name=t_user)), key=lambda x: x.id):
+            print('* tweet = {}'.format(status))
 
 if __name__ == '__main__':
-    work()
+    Twitter2Stdout.main()
