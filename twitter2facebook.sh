dd35a7b38bd0a7594aa372037d67d29db53e6edb
Gea-Suan Lin <gslin@gslin.org>
2022-10-23 10:09:56 +0800

    Refine the shell script.
    Remove flock from the shell script, which should be sit in crontab, not
    inside this shell script.

--- twitter2facebook.sh
+++ twitter2facebook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 LANG=en_US.UTF-8 \
-    /usr/bin/flock -n /tmp/twitter2facebook.lock \
-    ~/.pyenv/shims/python3 ~/git/twitter2facebook/twitter2facebook.py
+    ~/.pyenv/shims/python3 ~/git/twitter2facebook/twitter2facebook.py \
+    true
 
 # Since our pkill run with parent pid filtering (pid == 1), we need to
 # kill geckodriver first, then firefox-esr afterwards.
