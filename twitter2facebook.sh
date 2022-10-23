21e88621d6319d610e7eddad0c51ce3bf4c66779
Gea-Suan Lin <gslin@gslin.org>
2022-10-23 10:21:01 +0800

    It's ppid not pid.

--- twitter2facebook.sh
+++ twitter2facebook.sh
@@ -4,7 +4,7 @@ LANG=en_US.UTF-8 \
     ~/.pyenv/shims/python3 ~/git/twitter2facebook/twitter2facebook.py || \
     true
 
-# Since our pkill run with parent pid filtering (pid == 1), we need to
+# Since our pkill run with parent pid filtering (ppid == 1), we need to
 # kill geckodriver first, then firefox-esr afterwards.
 pkill -P 1 geckodriver || true
 pkill -P 1 firefox-esr || true
