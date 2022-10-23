48c8ff501f6402976eeb082938761a8edc003834
Gea-Suan Lin <gslin@gslin.org>
2022-10-23 10:12:40 +0800

    Add a missing "||".

--- twitter2facebook.sh
+++ twitter2facebook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LANG=en_US.UTF-8 \
-    ~/.pyenv/shims/python3 ~/git/twitter2facebook/twitter2facebook.py \
+    ~/.pyenv/shims/python3 ~/git/twitter2facebook/twitter2facebook.py || \
     true
 
 # Since our pkill run with parent pid filtering (pid == 1), we need to
