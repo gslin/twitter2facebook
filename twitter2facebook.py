8bbb2eb28259e35e8609c6ce78b82c2755668527
Gea-Suan Lin <gslin@gslin.org>
2023-05-25 08:18:38 +0800

    Migrate to use RSS-Bridge.

--- twitter2facebook.py
+++ twitter2facebook.py
@@ -9,7 +9,6 @@ import selenium.webdriver.firefox.options
 import sentry_sdk
 import sqlite3
 import time
-import twitter
 import urllib
 
 from selenium.webdriver.common.by import By
@@ -59,41 +58,27 @@ class Twitter2Facebook(object):
             sentry_sdk_url = c['default']['sentry_sdk_url']
             sentry_sdk.init(sentry_sdk_url)
 
-        t_ak = c['default']['twitter_access_token_key']
-        t_as = c['default']['twitter_access_token_secret']
-        t_ck = c['default']['twitter_consumer_key']
-        t_cs = c['default']['twitter_consumer_secret']
-        t_user = c['default']['twitter_username']
-        t = twitter.Api(access_token_key=t_ak, access_token_secret=t_as, consumer_key=t_ck, consumer_secret=t_cs)
+        jsonfeed_url = c['default']['twitter_rssbridge_jsonfeed_url']
+        items = requests.get(jsonfeed_url).json()['items']
 
         s = sqlite3.connect(f_db)
 
         sql_insert = 'INSERT INTO entry (twitter_id, created_at) VALUES (?, ?);'
         sql_select = 'SELECT COUNT(*) FROM entry WHERE twitter_id = ?;'
 
-        for status in sorted(list(t.GetUserTimeline(screen_name=t_user)), key=lambda x: x.id):
-            # Generate "text" with unescape (workaround).
-            text = html.unescape(status.text)
-            for u in status.urls:
-                text = text.replace(u.url, u.expanded_url)
-
-            # Skip if it's a retweet.
-            if status.retweeted:
+        for item in items:
+            # Skip if it's retweet.
+            if item['title'].startswith('@'):
                 continue
 
-            # Skip if it's from IFTTT and it contains Instagram url.
-            try:
-                if 'IFTTT' in status.source and 'https://instagr.am/p/' in status.urls[0].expanded_url:
-                    continue
-            except IndexError:
-                pass
-
-            # Generate "url"
-            url = 'https://twitter.com/{}/status/{}'.format(urllib.parse.quote(t_user), urllib.parse.quote(status.id_str))
+            # Generate parameters.
+            id_str = item['url'].split('/')[-1]
+            text = item['title']
+            url = item['url']
 
             c = s.cursor()
 
-            c.execute(sql_select, (status.id_str, ))
+            c.execute(sql_select, (id_str, ))
             if 0 == c.fetchone()[0]:
                 content = '{}\n\n{}'.format(text, url)
                 print('* content = {}'.format(content))
@@ -101,7 +86,7 @@ class Twitter2Facebook(object):
                 print(content)
                 self.post(content)
 
-                c.execute(sql_insert, (status.id_str, int(time.time())))
+                c.execute(sql_insert, (id_str, int(time.time())))
                 s.commit()
 
         self.quit_browser()
